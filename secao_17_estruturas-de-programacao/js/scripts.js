923a8145a8b7208ad9a13c456e5ecdb555a32ba8
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-18 22:52:19 -0300

    Estudando sobre o objeto matemático Math

--- secao_17_estruturas-de-programacao/js/scripts.js
+++ secao_17_estruturas-de-programacao/js/scripts.js
@@ -6,3 +6,21 @@ alert("O valor de z é: " + z);
 
 // O prompt exibe uma caixa de diálogo que solicita ao usuário que insira um valor
 const nome = prompt("Digite seu nome: ");
+
+// Math é um objeto embutido que fornece propriedades e métodos para constantes e funções matemáticas
+const raizQuadrada = Math.sqrt(16);
+console.log("A raiz quadrada de 16 é: " + raizQuadrada);
+
+console.log(Math.max(10, 20, 5)); // Retorna o maior valor entre os argumentos fornecidos
+
+console.log(Math.min(10, 20, 5)); // Retorna o menor valor entre os argumentos fornecidos
+
+console.log(Math.random()); // Retorna um número pseudo-aleatório entre 0 (inclusive) e 1 (exclusivo)
+
+console.log(Math.round(4.7)); // Arredonda o número para o inteiro mais próximo
+
+console.log(Math.ceil(4.2)); // Arredonda o número para cima, para o próximo inteiro
+
+console.log(Math.floor(4.7)); // Arredonda o número para baixo, para o inteiro mais próximo
+
+console.log(Math.pow(2, 3)); // Calcula 2 elevado à potência de 3 (2^3)
