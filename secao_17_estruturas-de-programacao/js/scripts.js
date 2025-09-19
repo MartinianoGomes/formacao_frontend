f1aa8858acbfd2181202986feafb867cb5a9d45d
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-18 22:49:42 -0300

    Práticas - Aplicando conceitos sobre estruturas de programação em Javascript.

---
+++ secao_17_estruturas-de-programacao/js/scripts.js
@@ -0,0 +1,8 @@
+// O alert exibe uma caixa de diálogo com uma mensagem
+alert("Olá, mundo!");
+
+const z = 5;
+alert("O valor de z é: " + z);
+
+// O prompt exibe uma caixa de diálogo que solicita ao usuário que insira um valor
+const nome = prompt("Digite seu nome: ");
