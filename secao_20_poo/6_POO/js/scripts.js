b3255edd660e06f0a500fc63abc188e0216a4fb0
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-21 09:12:44 -0300

    Adicionando práticas com Classes.

--- secao_20_poo/6_POO/js/scripts.js
+++ secao_20_poo/6_POO/js/scripts.js
@@ -1,5 +1,4 @@
 // 1 - Objetos
-
 const pessoa = {
     nome: 'Martiniano',
     sobrenome: 'Gomes',
@@ -13,3 +12,31 @@ const pessoa = {
 console.log(pessoa.nome) // Retornará o nome do objeto.
 console.log(pessoa.sobrenome) // Retornará o sobrenome do objeto.
 console.log(pessoa.olaMundo()) // Retornará a string 'Olá mundo!'.
+
+// Getters e Setters
+export class Conta {
+    constructor(email, senha) {
+        this._email = email;
+        this._senha = senha;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(novoEmail) {
+        this._email = novoEmail;
+    }
+
+    get senha() {
+        return this._senha;
+    }
+
+    set senha(novaSenha) {
+        this._senha = novaSenha;
+    }
+
+    login() {
+        return 'Você está logado!'
+    }
+}
