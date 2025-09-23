545c08b1ffbc090f3e98c0856a5a4c4dbad20978
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-22 21:59:37 -0300

    Exceções

--- secao_21_depuracao/js/scripts.js
+++ secao_21_depuracao/js/scripts.js
@@ -3,48 +3,72 @@
 // Depurando com console.table
 // Depurando com console.time e console.timeEnd
 // Depurando com debugger
-for (i = 0; i < 10; i++) {
-    let a = 10;
-    a += i;
+// for (i = 0; i < 10; i++) {
+//     let a = 10;
+//     a += i;
 
-    console.log(a);
-    console.log(i);
-}
+//     console.log(a);
+//     console.log(i);
+// }
 
-// Debugger
-function funcao1() {
-    console.log('Inicio da função 1');
-    funcao2();
-    console.log('Fim da função 1');
-}
+// // Debugger
+// function funcao1() {
+//     console.log('Inicio da função 1');
+//     funcao2();
+//     console.log('Fim da função 1');
+// }
 
-debugger;
+// debugger;
 
-function funcao2() {
-    console.log('Inicio da função 2');
-    console.log('Fim da função 2');
-}
+// function funcao2() {
+//     console.log('Inicio da função 2');
+//     console.log('Fim da função 2');
+// }
 
-// Tratamento de dados por função
-function checkNumber(number) {
-    let result = Number(number);
+// // Tratamento de dados por função
+// function checkNumber(number) {
+//     let result = Number(number);
 
-    if (Number.isNaN(result)) {
-        console.error('O valor deve ser um número');
-    }
+//     if (Number.isNaN(result)) {
+//         console.error('O valor deve ser um número');
+//     }
 
-    console.log(`Sucesso! O número é ${result}.`);
+//     console.log(`Sucesso! O número é ${result}.`);
+
+//     return result;
+// }
+
+// checkNumber('a');
+// checkNumber(10);
+// checkNumber('20');
+
+// Exceptions - Exceções
+// let x = 10;
+
+// if (x > 5) {
+//     throw new Error('O valor de x não pode ser maior que 5');
+// }
+
+// try...catch
+function soma(a, b) {
+    let num1 = Number(a);
+    let num2 = Number(b);
+
+    let result = num1 + num2;
 
     return result;
 }
 
-checkNumber('a');
-checkNumber(10);
-checkNumber('20');
+function subtracao(num1, num2) {
+    let result = num1 - num2;
 
-// Exceptions - Exceções
-let x = 10;
+    return result;
+}
 
-if (x > 5) {
-    throw new Error('O valor de x não pode ser maior que 5');
+try {
+    let result = soma('2', 2);
+
+    console.log(result);
+} catch (error) {
+    console.log(`Houve um erro na execução do programa, verifique: ${error}`);
 }
