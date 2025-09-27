2e4023653a078ccc9404917ac8b73db88cad6ecd
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-27 16:44:45 -0300

    Promises

--- secao_22_programacao-assincrona/js/scripts.js
+++ secao_22_programacao-assincrona/js/scripts.js
@@ -7,11 +7,30 @@
 
 // console.log("Programa ainda não ecexutou. 2");
 
+
+
 // Função setInterval
-console.log("Programa ainda não ecexutou.");
+// console.log("Programa ainda não ecexutou.");
 
-setInterval(() => {
-    console.log("Intervalo assíncrono.");
-}, 3000);
+// setInterval(() => {
+//     console.log("Intervalo assíncrono.");
+// }, 3000);
 
-console.log("Programa ainda não ecexutou. 2");
+// console.log("Programa ainda não ecexutou. 2");
+
+
+
+// Promises
+let somatorioAssincrono = Promise.resolve(() => { 5 + 5 });
+
+somatorioAssincrono
+    .then((value) => {
+        console.log(`A soma é ${value}`);
+        return value;
+    })
+    .then((value) => {
+        value - 1;
+    })
+    .then((value) => {
+        console.log(`Agora é ${value}`);
+    })
