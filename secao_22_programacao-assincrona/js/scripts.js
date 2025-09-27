176e9bc0760cdd7e110c6ce96cb423d5578733e6
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-27 16:37:07 -0300

    Função assíncrona setInterval

--- secao_22_programacao-assincrona/js/scripts.js
+++ secao_22_programacao-assincrona/js/scripts.js
@@ -1,8 +1,17 @@
 // Função setTimeout
+// console.log("Programa ainda não ecexutou.");
+
+// setTimeout(() => {
+//     console.log("Requisição assíncrona.");
+// }, 2000);
+
+// console.log("Programa ainda não ecexutou. 2");
+
+// Função setInterval
 console.log("Programa ainda não ecexutou.");
 
-setTimeout(() => {
-    console.log("Requisição assíncrona.");
-}, 2000);
+setInterval(() => {
+    console.log("Intervalo assíncrono.");
+}, 3000);
 
 console.log("Programa ainda não ecexutou. 2");
