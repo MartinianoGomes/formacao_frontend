ece2e31bd361696ea1e321c365cf18221f045518
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-09-30 22:54:11 -0300

    Removendo consoles logs desnecessários

--- secao_22_programacao-assincrona/js/scripts.js
+++ secao_22_programacao-assincrona/js/scripts.js
@@ -35,6 +35,9 @@
 //         console.log(`Agora é ${value}`);
 //     })
 
+
+
+
 // Tratamento de erros nas Promises
 // O método resolve do objeto Promise resolve instruções. Você pode receber e tratar o resultado destas instruções
 // com o método then. Encadeando o método cath logo após o then, você pode tratar algum erro que venha ocorrer com o
@@ -63,9 +66,6 @@ function checkNumber(n) {
         }
     })
 }
-
-let a = checkNumber(20);
-let b = checkNumber(10);
 
 a.then((v) => console.log(`O resultado é ${v}`)).catch((err) => { console.log(`Um erro ocorreu: ${err}`) });
 b.then((v) => console.log(`O resultado é ${v}`)).catch((err) => { console.log(`Um erro ocorreu: ${err}`) });
